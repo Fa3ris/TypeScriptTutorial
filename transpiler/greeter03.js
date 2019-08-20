298a9dd0fdc3799d6c8276f4ed1d45feb37a44f0
FabriceT <fabrice.tsiava@artik-consulting.com>
2019-08-20 09:30:19 +0200

    initial commit

---
+++ transpiler/greeter03.js
@@ -0,0 +1,15 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + ". " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+var user = { firstName: "Fabrice", lastName: "Tsiava" };
+var student = new Student("Fabrice", "N", "Tsiava");
+document.body.textContent = greeter(user);
